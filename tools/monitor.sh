d7715af3b078144d139a09bcd5c3ec846faefef0
ZenZenVibe <13644560+jossets@users.noreply.github.com>
2019-03-29 17:28:44 +0100

    Create monitor.sh

---
+++ tools/monitor.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Use CAdvisor to monitor the server & docker containers
+#
+# http://localhost:8888
+
+docker run --volume=/:/rootfs:ro --volume=/var/run:/var/run:rw --volume=/sys:/sys:ro --volume=/var/lib/docker/:/var/lib/docker:ro  --publish=8888:8080  --detach=true --name=cadvisor  google/cadvisor:latest
