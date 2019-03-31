f3fbf18016e2917e098fe5fc93f5f4cba7e1ed13
ZenZenVibe <13644560+jossets@users.noreply.github.com>
2019-03-31 18:26:59 +0200

    Update challenge.sh

--- ctf-transfert/challenge.sh
+++ ctf-transfert/challenge.sh
@@ -10,4 +10,4 @@ EXPOSEDPORT=22
 echo Starting challenge_box_provider
 echo Logs in /tmp/challenge_box_provider.log
 #./challenge-box-provider -cmd $CMD -image $IMAGE -life $LIFE -listen $LISTEN -port $EXPOSEDPORT  &> /tmp/challenge_box_provider.log &
-./challenge-box-provider -cmd $CMD -image $IMAGE -life $LIFE -listen $LISTEN -port $EXPOSEDPORT  &
+./challenge-box-provider -cmd $CMD -image $IMAGE -life $LIFE -listen $LISTEN -port $EXPOSEDPORT  &> /dev/null &
