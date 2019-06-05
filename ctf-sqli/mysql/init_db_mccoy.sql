1041fd12b5366639e24286e2994b74ff7a1f2301
root <root@localhost.localdomain>
2019-06-05 17:16:58 +0200

    yolo

--- ctf-sqli/mysql/init_db_mccoy.sql
+++ ctf-sqli/mysql/init_db_mccoy.sql
@@ -7,6 +7,9 @@ insert into users (login,passwd) values ('flag_S3ri3ux_m4n_f4ut_f1ltr3r_l3s_1npu
 CREATE TABLE messages (id INT NOT NULL AUTO_INCREMENT, idmsg VARCHAR(45) NULL, msg VARCHAR(2000) NULL, PRIMARY KEY (id));
 insert into messages (idmsg,msg) values ('673489', 'flag{T0uj0urs_f1ltr3r_l3s_1nputs}'); 
 insert into messages (idmsg,msg) values ('892345', 'Penser à breveter le system d authentification');
+CREATE TABLE bonus (id INT NOT NULL AUTO_INCREMENT, idmsg VARCHAR(45) NULL, msg VARCHAR(2000) NULL, PRIMARY KEY (id));
+insert into bonus (idmsg,msg) values ('2121', 'flag{L4_C_3st_bi3n_r4ng3}'); 
+insert into bonus (idmsg,msg) values ('4242', 'Trop bien caché.. Tu me trouveras pas...');
 CREATE USER 'readuser'@'%' IDENTIFIED BY 'passwordforreaduser';
 GRANT SELECT ON dbmccoy.* TO 'readuser'@'%';
 FLUSH PRIVILEGES;
