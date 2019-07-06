0e47d49a2bc0ee762ec4b3cd838b259bffb08b33
yop <yop@myfirst_ctf.org>
2019-07-06 07:14:05 +0200

    yolo

--- web_server/www_site/yoloctf/ctf_sql.php
+++ web_server/www_site/yoloctf/ctf_sql.php
@@ -1,7 +1,7 @@
 <?php
     $passwd = getenv('PHP_ADMIN_ACCOUNT')?getenv('PHP_ADMIN_ACCOUNT'):'admin';
     //$mysqli = new mysqli("webserver_mysql", "root", "AZ56FG78HJZE34", "dbctf");
-    $mysqli = new mysqli("webserver_mysql", "ctfuser", "passwordforctfuser", "dbctf");
+    $mysqli = new mysqli("webserver_mysql", "ctfuser", $passwd, "dbctf");
     if (mysqli_connect_errno()) {
         printf("Connect failed: %s\n", mysqli_connect_error());
         exit();
