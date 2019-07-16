d90e4b9f9dbe98c953cf46d2155751604352a5cc
root <root@localhost.localdomain>
2019-07-16 18:24:40 +0200

    generate .env with random password

--- web_server/challenge_set_config.sh
+++ web_server/challenge_set_config.sh
@@ -3,6 +3,7 @@
 # Update config files based on .env variables
 #
 . .env
+
 cp mysql/init_db_ctf.sql.ori  mysql/init_db_ctf.sql
 sed -i -e "s/MYSQL_USER_PASSWORD/$MYSQL_USER_PASSWORD/g" mysql/init_db_ctf.sql
 sed -i -e "s/CTF_ADMIN_ACCOUNT/$CTF_ADMIN_ACCOUNT/g" mysql/init_db_ctf.sql
