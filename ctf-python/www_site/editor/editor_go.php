212252c32dc5adcd0a214a36da1dfff634b10b4f
jossets <13644560+jossets@users.noreply.github.com>
2019-07-19 20:28:06 +0200

    Add python challenge

--- ctf-python/www_site/editor/editor_go.php
+++ ctf-python/www_site/editor/editor_go.php
@@ -7,5 +7,10 @@ if (isset($_POST['code'])) {
     fclose($file );
     $output = shell_exec('python /tmp/code.py 2>&1');
     echo "$ python /tmp/code.py\n".$output;
+
+    $output_flag = "Yolo";
+    if (substr($output, 0, strlen($output_flag)) === $output_flag) { 
+        echo "Flag: Y0l0_l3s_p0t0";
+    };
 } 
 ?>
