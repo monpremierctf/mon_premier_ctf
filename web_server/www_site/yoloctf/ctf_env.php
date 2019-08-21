f90c88c254574e165e15b41c865c6821557ba6a9
root <root@localhost.localdomain>
2019-08-21 14:46:29 +0200

    Added mail

--- web_server/www_site/yoloctf/ctf_env.php
+++ web_server/www_site/yoloctf/ctf_env.php
@@ -2,7 +2,6 @@
     $admin = getenv('CTF_ADMIN_ACCOUNT')?getenv('CTF_ADMIN_ACCOUNT'):'admin';
     $passwd = getenv('MYSQL_USER_PASSWORD')?getenv('MYSQL_USER_PASSWORD'):'passwordforctfuser';
     $scoreboard_aff = getenv('CTF_SCOREBOARD_AFF')?getenv('CTF_SCOREBOARD_AFF'):'all';
-    $use_mail = getenv('CTF_USE_MAIL')?getenv('CTF_USE_MAIL'):'false';
     
     // mail
     $ctf_mail_enabled  = getenv('CTF_MAIL_ENABLED')?getenv('CTF_MAIL_ENABLED'):'false';
