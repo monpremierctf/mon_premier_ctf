9e24173146f43f7379a3ce9d0ceec5b01bbfbc30
root <root@localhost.localdomain>
2019-08-29 12:20:03 +0200

    Build VM

--- tools/vagrant-boxes/etc_init_d_mon_premier_ctf.sh
+++ tools/vagrant-boxes/etc_init_d_mon_premier_ctf.sh
@@ -1,20 +1,32 @@
 
-#!/bin/sh
+#!/bin/sh -e
 
 ### BEGIN INIT INFO
 # Provides:		mon_premier_ctf
-# Required-Start:	
+# Required-Start:	$all
 # Required-Stop:	
 # Default-Start:	2 3 4 5
-# Default-Stop:		
+# Default-Stop:		0 1 6
 # Short-Description:	Mon premier CTF
 ### END INIT INFO
 
+# $ service --status-all
+# $ sudo service mon_premier_ctf.sh start
+# $ service mon_premier_ctf.sh status
+
 set -e
+
+DAEMON="/home/vagrant/mon_premier_ctf/go_first_install_webserver_run" #ligne de commande du programme.
+daemon_OPT="-y"  #argument à utiliser par le programme.
+DAEMONDIR="/home/vagrant/mon_premier_ctf" 
+DAEMONUSER="vagrant" #utilisateur du programme
+daemon_NAME="go_first_install_webserver_run" #Nom du programme (doit être identique à l'exécutable).
+
+
+
 case "$1" in
   start)
-	cd /home/vagrant/mon_premier_ctf; 
-    ./go_first_install_webserver_run -y
+    start-stop-daemon --background --name $daemon_NAME --start --quiet --chdir $DAEMONDIR --chuid $DAEMONUSER --exec $DAEMON -- $daemon_OPT
 	;;
   stop)
 	;;
