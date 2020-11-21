5ce8c02e8b03e327ce8741c679220852e5a6e739
ZenZenVibe <13644560+jossets@users.noreply.github.com>
2020-11-21 09:25:03 +0100

    Create downld_from_googledrive_ova.sh

---
+++ downld_from_googledrive_ova.sh
@@ -0,0 +1,6 @@
+echo '== Download monpremierctf.iso from googledrive =='
+ggID='1LvcRQ8aUUXzW4xIUsc8UmsR_kseuti4i'
+ggURL='https://drive.google.com/uc?export=download'
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
